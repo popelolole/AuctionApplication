6760e12ca473e2af44ef82806cb074272dc97553
popelolole <pellebe@kth.se>
2023-10-13 15:55:57 +0200

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AuctionApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
